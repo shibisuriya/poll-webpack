790b9f364b6bf259005a4ade342f4d19dd7c4782
SAKTHI S M <smsakthi812@gmail.com>
2024-04-24 17:36:33 +0530

    Ui

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,18 +1,6 @@
-import React, { Fragment } from "react";
+import React from "react";
+import Builder from "./builder";
 
 export default function App() {
-  let name = "مرحبا بك";
-  return (
-    <Fragment>
-      <div>
-        {name} 1. This is my name
-      </div>
-      <div>
-        <bdi>{name}</bdi> 1. This is my name
-      </div>
-      <div>
-        <span style={{ unicodeBidi: "isolate" }}>{name}</span> 4
-      </div>
-    </Fragment>
-  );
+  return <Builder />;
 }
